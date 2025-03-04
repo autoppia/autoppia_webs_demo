f8e3eaee9e860616ee45921c2dd626aea0eb7c9b
roberto mazo <mazziitoozz@gmail.com>
2025-03-04 18:25:46 +0100

    update

--- web_3_demo_movies/entrypoint.sh
+++ web_3_demo_movies/entrypoint.sh
@@ -4,11 +4,11 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
+# 3. Seedear la base de datos
+python manage.py seed_movies
 # 2. Crear usuario de prueba
 python manage.py create_user
 
-# 3. Seedear la base de datos
-python manage.py seed_movies
 
 # 4. Arrancar Gunicorn en el puerto 8003
 exec gunicorn --bind 0.0.0.0:8003 movieproject.wsgi:application
