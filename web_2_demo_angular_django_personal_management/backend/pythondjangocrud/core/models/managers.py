fef863cd716216fad63857f460421f86d140cc22
mingchoi1126 <mingchoi931126@gmail.com>
2025-03-25 23:52:57 -0400

    Demo books

--- web_2_demo_angular_django_personal_management/backend/pythondjangocrud/core/models/managers.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class CoreManager(models.Manager):
-    def actives(self):
-        return super(CoreManager, self).get_queryset().filter(is_active=True)
--- web_3_demo_django_jobs/config.py
+++
@@ -1,6 +0,0 @@
-from events.events import EVENTS_ALLOWED
-
-DEMO_WEB_PROJECT_CONFIG = {
-    "name" : "Jobs Portal",
-    "events" : EVENTS_ALLOWED
-}
