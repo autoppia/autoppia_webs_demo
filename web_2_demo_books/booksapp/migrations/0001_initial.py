23a7d54b4b7eabe02efe0cd90d6f6d9fec84b591
Gul Shair <Gul.shair@rev9solutions.com>
2025-04-15 13:02:17 +0500

    bug fix

--- web_2_demo_books/booksapp/migrations/0001_initial.py
+++ web_2_demo_books/booksapp/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -16,84 +15,87 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Book',
+            name="Book",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('userId', models.BigIntegerField()),
-                ('name', models.CharField(max_length=250)),
-                ('desc', models.TextField()),
-                ('year', models.IntegerField(validators=[django.core.validators.MinValueValidator(1900), django.core.validators.MaxValueValidator(2100)])),
-                ('img', models.ImageField(blank=True, null=True, upload_to='gallery')),
-                ('director', models.CharField(blank=True, max_length=250, null=True)),
-                ('duration', models.IntegerField(blank=True, help_text='Duration in minutes', null=True)),
-                ('trailer_url', models.URLField(blank=True, null=True)),
-                ('rating', models.FloatField(default=0.0, help_text='Rating between 0 and 5', validators=[django.core.validators.MinValueValidator(0.0), django.core.validators.MaxValueValidator(5.0)])),
-                ('price', models.FloatField(default=0)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("userId", models.BigIntegerField()),
+                ("name", models.CharField(max_length=250)),
+                ("desc", models.TextField()),
+                ("year", models.IntegerField(validators=[django.core.validators.MinValueValidator(1900), django.core.validators.MaxValueValidator(2100)])),
+                ("img", models.ImageField(blank=True, null=True, upload_to="gallery")),
+                ("director", models.CharField(blank=True, max_length=250, null=True)),
+                ("duration", models.IntegerField(blank=True, help_text="Duration in minutes", null=True)),
+                ("trailer_url", models.URLField(blank=True, null=True)),
+                (
+                    "rating",
+                    models.FloatField(default=0.0, help_text="Rating between 0 and 5", validators=[django.core.validators.MinValueValidator(0.0), django.core.validators.MaxValueValidator(5.0)]),
+                ),
+                ("price", models.FloatField(default=0)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
             ],
             options={
-                'ordering': ['-created_at'],
+                "ordering": ["-created_at"],
             },
         ),
         migrations.CreateModel(
-            name='Cart',
+            name="Cart",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('userId', models.BigIntegerField()),
-                ('bookId', models.BigIntegerField()),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("userId", models.BigIntegerField()),
+                ("bookId", models.BigIntegerField()),
             ],
         ),
         migrations.CreateModel(
-            name='ContactMessage',
+            name="ContactMessage",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('email', models.EmailField(max_length=254)),
-                ('subject', models.CharField(max_length=200)),
-                ('message', models.TextField()),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=100)),
+                ("email", models.EmailField(max_length=254)),
+                ("subject", models.CharField(max_length=200)),
+                ("message", models.TextField()),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
             ],
             options={
-                'ordering': ['-created_at'],
+                "ordering": ["-created_at"],
             },
         ),
         migrations.CreateModel(
-            name='Genre',
+            name="Genre",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=100)),
             ],
         ),
         migrations.CreateModel(
-            name='Comment',
+            name="Comment",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('content', models.TextField()),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('avatar', models.ImageField(blank=True, null=True, upload_to='gallery/avatars')),
-                ('movie', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='booksapp.book')),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=100)),
+                ("content", models.TextField()),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("avatar", models.ImageField(blank=True, null=True, upload_to="gallery/avatars")),
+                ("movie", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="comments", to="booksapp.book")),
             ],
             options={
-                'ordering': ['-created_at'],
+                "ordering": ["-created_at"],
             },
         ),
         migrations.AddField(
-            model_name='book',
-            name='genres',
-            field=models.ManyToManyField(blank=True, related_name='movies', to='booksapp.genre'),
+            model_name="book",
+            name="genres",
+            field=models.ManyToManyField(blank=True, related_name="movies", to="booksapp.genre"),
         ),
         migrations.CreateModel(
-            name='UserProfile',
+            name="UserProfile",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('bio', models.TextField(blank=True, max_length=500, null=True)),
-                ('profile_pic', models.ImageField(blank=True, null=True, upload_to='gallery/profiles')),
-                ('website', models.URLField(blank=True, null=True)),
-                ('location', models.CharField(blank=True, max_length=100, null=True)),
-                ('favorite_genres', models.ManyToManyField(blank=True, related_name='user_favorites', to='booksapp.genre')),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to=settings.AUTH_USER_MODEL)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("bio", models.TextField(blank=True, max_length=500, null=True)),
+                ("profile_pic", models.ImageField(blank=True, null=True, upload_to="gallery/profiles")),
+                ("website", models.URLField(blank=True, null=True)),
+                ("location", models.CharField(blank=True, max_length=100, null=True)),
+                ("favorite_genres", models.ManyToManyField(blank=True, related_name="user_favorites", to="booksapp.genre")),
+                ("user", models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name="profile", to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
