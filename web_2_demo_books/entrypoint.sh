a70e2e61ede061a95351eb5a47f4d255355ff2c6
Gul Shair <Gul.shair@rev9solutions.com>
2025-04-16 15:47:38 +0500

    entrypoint updated

--- web_2_demo_books/entrypoint.sh
+++ web_2_demo_books/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# 1. Execute migrations in SQLite
+# 1. Execute migrations
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-# 2. Seed users (with optional parameters if needed)
-python manage.py seed_users --start=1 --end=255 --prefix="user" --password="PASSWORD"
+# 2. Seed users with books
+python manage.py seed_users_with_books --start=1 --end=255 --prefix="user" --password="PASSWORD"
 
-# 3. Seed movies
-python manage.py seed_books
-
-# 4. Start Gunicorn on port 8000 (note this was incorrectly commented as 8003)
+# 3. Start Gunicorn on port 8001
 exec gunicorn --bind 0.0.0.0:8001 booksproject.wsgi:application
