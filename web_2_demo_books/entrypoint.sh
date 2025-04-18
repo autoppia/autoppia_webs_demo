ad5a06684d066353c11a7b1b97ca4a598595abec
Gul Shair <Gul.shair@rev9solutions.com>
2025-04-18 17:55:10 +0500

    entrypoint updated

--- web_2_demo_books/entrypoint.sh
+++ web_2_demo_books/entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 # 2. Seed users with books
-python manage.py seed_users_with_books --start=1 --end=255 --prefix="user" --password="PASSWORD"
+python manage.py seed_users_with_books --start=1 --end=256 --prefix="user" --password="PASSWORD"
 
 # 3. Start Gunicorn on port 8001
 exec gunicorn --bind 0.0.0.0:8001 booksproject.wsgi:application
