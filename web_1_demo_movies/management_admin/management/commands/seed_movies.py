371b4339180ce6513b0b749af84ca235fff0d576
Gul Shair <Gul.shair@rev9solutions.com>
2025-05-05 16:38:46 +0500

    web1 added

---
+++ web_1_demo_movies/management_admin/management/commands/seed_movies.py
@@ -0,0 +1,337 @@
+import os
+import random
+from datetime import timedelta
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.utils.text import slugify
+
+from movieapp.models import Movie, Genre, Comment
+from django.apps import apps 
+
+class Command(BaseCommand):
+    help = "Resets ALL tables and then seeds the database with initial movies, genres, and comments"
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS("Starting to seed the database..."))
+        all_models = apps.get_models()  
+        for model in all_models:
+            model.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS("All tables have been reset (all records deleted)."))
+        # Create genres
+        genres_to_create = [
+            "Action", "Adventure", "Animation", "Comedy", "Crime", "Biography",
+            "Documentary", "Drama", "Fantasy", "Horror", "Mystery",
+            "Romance", "Sci-Fi", "Thriller", "War", "Western"
+        ]
+        
+        genres = {}
+        for genre_name in genres_to_create:
+            genre, created = Genre.objects.get_or_create(name=genre_name)
+            genres[genre_name] = genre
+            status = "Created" if created else "Already exists"
+            self.stdout.write(f"{status}: Genre '{genre_name}'")
+        
+        # Sample movie data (usando nombres de archivos locales en vez de URLs)
+        movies_data = [
+            {
+                "name": "The Shawshank Redemption",
+                "desc": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                "year": 1994,
+                "director": "Frank Darabont",
+                "cast": "Tim Robbins, Morgan Freeman, Bob Gunton",
+                "duration": 142,
+                "trailer_url": "https://www.youtube.com/watch?v=6hB3S9bIaco",
+                "rating": 4.8,
+                "img_file": "the-shawshank-redemption.jpg",  # <-- archivo local
+                "genres": ["Drama"]
+            },
+            {
+                "name": "The Godfather",
+                "desc": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+                "year": 1972,
+                "director": "Francis Ford Coppola",
+                "cast": "Marlon Brando, Al Pacino, James Caan",
+                "duration": 175,
+                "trailer_url": "https://www.youtube.com/watch?v=sY1S34973zA",
+                "rating": 4.7,
+                "img_file": "the-godfather.jpg",
+                "genres": ["Crime", "Drama"]
+            },
+            {
+                "name": "The Dark Knight",
+                "desc": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+                "year": 2008,
+                "director": "Christopher Nolan",
+                "cast": "Christian Bale, Heath Ledger, Aaron Eckhart",
+                "duration": 152,
+                "trailer_url": "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+                "rating": 4.7,
+                "img_file": "the-dark-knight.jpg",
+                "genres": ["Action", "Crime", "Drama", "Thriller"]
+            },
+            {
+                "name": "Pulp Fiction",
+                "desc": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+                "year": 1994,
+                "director": "Quentin Tarantino",
+                "cast": "John Travolta, Uma Thurman, Samuel L. Jackson",
+                "duration": 154,
+                "trailer_url": "https://www.youtube.com/watch?v=s7EdQ4FqbhY",
+                "rating": 4.6,
+                "img_file": "pulp-fiction.jpg",
+                "genres": ["Crime", "Drama"]
+            },
+            {
+                "name": "Inception",
+                "desc": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+                "year": 2010,
+                "director": "Christopher Nolan",
+                "cast": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+                "duration": 148,
+                "trailer_url": "https://www.youtube.com/watch?v=YoHD9XEInc0",
+                "rating": 4.5,
+                "img_file": "inception.jpg",
+                "genres": ["Action", "Adventure", "Sci-Fi", "Thriller"]
+            },
+            {
+                "name": "The Lord of the Rings: The Fellowship of the Ring",
+                "desc": "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+                "year": 2001,
+                "director": "Peter Jackson",
+                "cast": "Elijah Wood, Ian McKellen, Orlando Bloom",
+                "duration": 178,
+                "trailer_url": "https://www.youtube.com/watch?v=V75dMMIW2B4",
+                "rating": 4.6,
+                "img_file": "the-lord-of-the-rings-the-fellowship-of-the-ring.jpg",
+                "genres": ["Action", "Adventure", "Drama", "Fantasy"]
+            },
+            {
+                "name": "Forrest Gump",
+                "desc": "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+                "year": 1994,
+                "director": "Robert Zemeckis",
+                "cast": "Tom Hanks, Robin Wright, Gary Sinise",
+                "duration": 142,
+                "trailer_url": "https://www.youtube.com/watch?v=bLvqoHBptjg",
+                "rating": 4.6,
+                "img_file": "forrest-gump.jpg",
+                "genres": ["Drama", "Romance"]
+            },
+            {
+                "name": "The Matrix",
+                "desc": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+                "year": 1999,
+                "director": "Lana Wachowski, Lilly Wachowski",
+                "cast": "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
+                "duration": 136,
+                "trailer_url": "https://www.youtube.com/watch?v=vKQi3bBA1y8",
+                "rating": 4.5,
+                "img_file": "the-matrix.jpg",
+                "genres": ["Action", "Sci-Fi"]
+            },
+            {
+                "name": "Goodfellas",
+                "desc": "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
+                "year": 1990,
+                "director": "Martin Scorsese",
+                "cast": "Robert De Niro, Ray Liotta, Joe Pesci",
+                "duration": 146,
+                "trailer_url": "https://www.youtube.com/watch?v=qo5jJpHtI1Y",
+                "rating": 4.6,
+                "img_file": "goodfellas.jpg",
+                "genres": ["Biography", "Crime", "Drama"]
+            },
+            {
+                "name": "Interestellar",
+                "desc": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                "year": 2014,
+                "director": "Christopher Nolan",
+                "cast": "Matthew McConaughey, Anne Hathaway, Jessica Chastain",
+                "duration": 169,
+                "trailer_url": "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+                "rating": 4.5,
+                "img_file": "interstellar.jpg",
+                "genres": ["Adventure", "Drama", "Sci-Fi"]
+            }
+        ]
+
+        # Sample comment data
+        sample_comments = {
+            "male_names": [
+                "James", "Michael", "William", "Daniel", "David", "Robert", "John", "Thomas",
+                "Matthew", "Christopher", "Joseph", "Andrew", "Edward", "Mark", "Brian", "Steven",
+                "Kevin", "Jason", "Timothy", "Jeffrey", "Ryan", "Jacob", "Gary", "Nicholas",
+                "Eric", "Jonathan", "Stephen", "Justin", "Charles", "Anthony", "Richard", "Scott"
+            ],
+            "female_names": [
+                "Emma", "Sophia", "Olivia", "Ava", "Isabella", "Mia", "Charlotte", "Amelia",
+                "Harper", "Evelyn", "Abigail", "Emily", "Elizabeth", "Sofia", "Madison", "Avery",
+                "Ella", "Scarlett", "Grace", "Victoria", "Lily", "Samantha", "Eleanor", "Hannah",
+                "Lillian", "Addison", "Aubrey", "Layla", "Ellie", "Stella", "Natalie", "Zoe", "Leah", "Haley"
+            ],
+            "positive_comments": [
+                "Absolutely loved this movie! A masterpiece that stands the test of time.",
+                "One of the best films I've ever seen. The acting was phenomenal.",
+                "Incredible storytelling and direction. This movie deserves all the praise!",
+                "The cinematography was breathtaking. Every frame looked like a painting.",
+                "A perfect blend of emotion and technical brilliance.",
+                "This film had me on the edge of my seat the entire time!",
+                "I've watched this multiple times and it gets better with each viewing.",
+                "The score perfectly complements the storytelling. A true classic!",
+                "Masterful performances by the entire cast. Truly unforgettable.",
+                "This movie changed my perspective on cinema. Absolutely brilliant."
+            ],
+            "mixed_comments": [
+                "Good film overall, though some scenes dragged on a bit too long.",
+                "Solid performances, but the plot had a few holes I couldn't ignore.",
+                "Visually stunning, but the character development felt a bit weak.",
+                "Enjoyed it, but I think it's slightly overrated in some aspects.",
+                "A good movie that could have been great with some tighter editing.",
+                "Interesting concept but the execution was somewhat inconsistent.",
+                "Worth watching, though I expected a bit more given all the hype.",
+                "Some brilliant moments mixed with a few that didn't quite land."
+            ],
+            "critical_comments": [
+                "Not my cup of tea. I found the pacing to be too slow.",
+                "The plot was confusing and hard to follow at times.",
+                "I expected more given the high ratings. A bit disappointing.",
+                "The characters weren't believable enough for me to get invested.",
+                "Technically well-made but emotionally distant."
+            ]
+        }
+        
+        # First check if movies already exist
+        auto_confirm = os.environ.get('AUTO_SEED', 'false').lower() == 'true'
+        
+        if Movie.objects.count() > 0:
+            if auto_confirm:
+                Comment.objects.all().delete()
+                Movie.objects.all().delete()
+                self.stdout.write(self.style.SUCCESS("All existing movies and comments deleted."))
+            else:
+                self.stdout.write(self.style.WARNING(
+                    "Movies already exist in the database. "
+                    "Do you want to delete all existing movies and comments? (yes/no)"
+                ))
+                confirm = input()
+                if confirm.lower() != 'yes':
+                    self.stdout.write(self.style.SUCCESS("Seeding cancelled."))
+                    return
+                else:
+                    Comment.objects.all().delete()
+                    Movie.objects.all().delete()
+                    self.stdout.write(self.style.SUCCESS("All existing movies and comments deleted."))
+        
+        # Create movies
+        created_movies = []
+        for movie_data in movies_data:
+            try:
+                # Nombre de archivo local que YA tienes en media/gallery
+                file_name = movie_data['img_file']
+                
+                # Ruta absoluta hasta donde está tu imagen en media/gallery
+                local_path = os.path.join(settings.MEDIA_ROOT, "gallery", file_name)
+                
+                # Creamos la instancia del Movie
+                movie = Movie(
+                    name=movie_data['name'],
+                    desc=movie_data['desc'],
+                    year=movie_data['year'],
+                    director=movie_data['director'],
+                    cast=movie_data['cast'],
+                    duration=movie_data['duration'],
+                    trailer_url=movie_data['trailer_url'],
+                    rating=movie_data['rating']
+                )
+                movie.save()
+                
+                # Asignamos la imagen local (asumiendo que existe en media/gallery)
+                if os.path.exists(local_path):
+                    # Ajusta la ruta relativa para que Django la maneje
+                    movie.img = f"gallery/{file_name}"
+                    movie.save()
+                    self.stdout.write(self.style.SUCCESS(
+                        f"Using local image: Movie '{movie_data['name']}'"
+                    ))
+                else:
+                    self.stdout.write(self.style.WARNING(
+                        f"Image not found for '{movie_data['name']}': {local_path}"
+                    ))
+                
+                # Añadimos géneros
+                for genre_name in movie_data['genres']:
+                    if genre_name in genres:
+                        movie.genres.add(genres[genre_name])
+                
+                created_movies.append(movie)
+                
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(
+                    f"Error creating movie {movie_data['name']}: {e}"
+                ))
+        
+        # Add comments to movies
+        self.stdout.write(self.style.SUCCESS("Adding comments to movies..."))
+        
+        male_names = sample_comments["male_names"]
+        female_names = sample_comments["female_names"]
+        positive_comments = sample_comments["positive_comments"]
+        mixed_comments = sample_comments["mixed_comments"]
+        critical_comments = sample_comments["critical_comments"]
+        
+        now = timezone.now()
+        
+        # Define avatar associations - person1.jpg and person4.jpg are female, person2.png and person3.jpg are male
+        female_avatars = ["person1.jpg", "person4.jpg"]
+        male_avatars = ["person2.png", "person3.jpg"]
+        
+        for movie in created_movies:
+            # Determine number of comments for this movie (3-8)
+            num_comments = random.randint(3, 8)
+            
+            # Generate comments with different sentiment distributions based on rating
+            if movie.rating >= 4.5:
+                sentiment_weights = [0.8, 0.15, 0.05]  # 80% positive, 15% mixed, 5% critical
+            elif movie.rating >= 4.0:
+                sentiment_weights = [0.6, 0.3, 0.1]    # 60% positive, 30% mixed, 10% critical
+            else:
+                sentiment_weights = [0.4, 0.4, 0.2]    # 40% positive, 40% mixed, 20% critical
+            
+            for _ in range(num_comments):
+                sentiment = random.choices(
+                    ["positive", "mixed", "critical"], 
+                    weights=sentiment_weights, 
+                    k=1
+                )[0]
+                
+                if sentiment == "positive":
+                    comment_text = random.choice(positive_comments)
+                elif sentiment == "mixed":
+                    comment_text = random.choice(mixed_comments)
+                else:
+                    comment_text = random.choice(critical_comments)
+                
+                # Create a random date in the past 60 days
+                days_ago = random.randint(1, 60)
+                comment_date = now - timedelta(days=days_ago)
+                
+                # Randomly select gender and name
+                use_male = random.choice([True, False])
+                if use_male:
+                    commenter_name = random.choice(male_names)
+                    avatar_file = f"gallery/people/{random.choice(male_avatars)}"
+                else:
+                    commenter_name = random.choice(female_names)
+                    avatar_file = f"gallery/people/{random.choice(female_avatars)}"
+                
+                Comment.objects.create(
+                    movie=movie,
+                    name=commenter_name,
+                    content=comment_text,
+                    created_at=comment_date
+                )
+            
+            self.stdout.write(f"Added {num_comments} comments to '{movie.name}'")
+        
+        self.stdout.write(self.style.SUCCESS("Database seeding completed successfully!"))
---
+++ web_1_demo_movies/management_admin/management/commands/seed_users.py
@@ -0,0 +1,226 @@
+import random
+import time
+import concurrent.futures
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.db import transaction
+from movieapp.models import UserProfile, Genre
+
+
+class Command(BaseCommand):
+    help = "Seed many test users quickly using bulk_create, with optional parallelization."
+
+    def add_arguments(self, parser):
+        parser.add_argument('--start', type=int, default=1,
+                            help='Starting user number (default: 1)')
+        parser.add_argument('--end', type=int, default=255,
+                            help='Ending user number (default: 255)')
+        parser.add_argument('--prefix', type=str, default='user',
+                            help='Username prefix (default: "user")')
+        parser.add_argument('--password', type=str, default='password123',
+                            help='Plain-text password for all users (default: "password123")')
+        parser.add_argument('--profile-pic', type=str, default='/media/gallery/people/default_profile.jpg',
+                            help='Path/URL of profile pic for all users (default: "/media/gallery/people/default_profile.jpg")')
+        parser.add_argument('--batch-size', type=int, default=500,
+                            help='Number of users per batch (default: 500)')
+        parser.add_argument('--workers', type=int, default=4,
+                            help='Number of parallel workers for seeding (default: 4)')
+
+    def handle(self, *args, **options):
+        start_num = options['start']
+        end_num = options['end']
+        prefix = options['prefix']
+        plain_password = options['password']
+        profile_pic_url = options['profile_pic']
+        batch_size = options['batch_size']
+        max_workers = options['workers']
+
+        total_to_create = end_num - start_num + 1
+        self.stdout.write(f"\nSeeding {total_to_create} users in batches of {batch_size}...")
+
+        # Pre-hash the password once (dummy hashed password for all users)
+        # This drastically reduces CPU overhead from repeated password hashing.
+        prehashed_password = make_password(plain_password)
+
+        # Gather all genres for random assignment (optional).
+        all_genres = list(Genre.objects.all())
+
+        # Build batch ranges
+        batches = []
+        for bstart in range(start_num, end_num + 1, batch_size):
+            bend = min(bstart + batch_size - 1, end_num)
+            batches.append((bstart, bend))
+
+        # Start timer
+        start_time = time.time()
+
+        # We'll collect aggregated results here
+        aggregated = {
+            "created": 0,
+            "skipped": 0,
+            "errors": 0
+        }
+
+        self.stdout.write(f"Running with up to {max_workers} parallel workers...\n")
+
+        # Use ThreadPoolExecutor or ProcessPoolExecutor.
+        # ThreadPool is often enough, but if you suspect CPU-bound tasks, you may try ProcessPool.
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            future_to_batch = {}
+            for (bstart, bend) in batches:
+                future = executor.submit(
+                    self.process_batch,
+                    bstart,
+                    bend,
+                    prefix,
+                    prehashed_password,
+                    profile_pic_url,
+                    all_genres
+                )
+                future_to_batch[future] = (bstart, bend)
+
+            completed = 0
+            total_batches = len(batches)
+            for future in concurrent.futures.as_completed(future_to_batch):
+                (bstart, bend) = future_to_batch[future]
+                try:
+                    res = future.result()
+                    aggregated["created"] += res["created"]
+                    aggregated["skipped"] += res["skipped"]
+                    aggregated["errors"] += res["errors"]
+                except Exception as e:
+                    self.stderr.write(
+                        f"ERROR in batch {bstart}-{bend}: {repr(e)}"
+                    )
+                    # If there's an unhandled exception, assume the entire batch is an error
+                    aggregated["errors"] += (bend - bstart + 1)
+
+                completed += 1
+                self.stdout.write(f" --> Completed {completed}/{total_batches} batches.")
+
+        duration = time.time() - start_time
+        self.stdout.write("\n--- Seeding Summary ---")
+        self.stdout.write(f"Total created: {aggregated['created']}")
+        self.stdout.write(f"Total skipped: {aggregated['skipped']}")
+        self.stdout.write(f"Total errors:  {aggregated['errors']}")
+        self.stdout.write(f"Time taken:    {duration:.2f}s\n")
+
+        self.stdout.write(self.style.SUCCESS("Seeding complete."))
+
+    def process_batch(self, start_idx, end_idx, prefix, prehashed_password, profile_pic_url, all_genres):
+        """
+        Process a single batch of users:
+          1) Check which of them already exist (skip those).
+          2) Bulk-create the rest in a single transaction.
+          3) Bulk-create corresponding UserProfile rows.
+          4) Optionally assign random favorite genres.
+        Returns dict with {created, skipped, errors}.
+        """
+        result = {
+            "created": 0,
+            "skipped": 0,
+            "errors": 0
+        }
+
+        # Build the list of prospective usernames
+        usernames_in_batch = [
+            f"{prefix}{i}"
+            for i in range(start_idx, end_idx + 1)
+        ]
+
+        # Find which of these already exist
+        existing_usernames = set(
+            User.objects.filter(
+                username__in=usernames_in_batch
+            ).values_list('username', flat=True)
+        )
+
+        # Build up the list of user objects that do NOT exist yet
+        users_to_create = []
+        # We also keep track of (username -> i) index to help with building profile data
+        user_index_map = {}
+
+        for i in range(start_idx, end_idx + 1):
+            username = f"{prefix}{i}"
+            if username in existing_usernames:
+                result["skipped"] += 1
+                continue
+            user_index_map[username] = i
+            users_to_create.append(
+                User(
+                    username=username,
+                    email=f"{username}@example.com",
+                    password=prehashed_password,
+                    first_name=f"Test{i}",
+                    last_name=f"User{i}"
+                )
+            )
+
+        if not users_to_create:
+            # Nothing new to create in this batch
+            return result
+
+        # We'll pick random data for profiles
+        bio_templates = [
+            "I'm a passionate movie enthusiast who loves {genre1} and {genre2}.",
+            "Cinephile with a taste for {genre1} and {genre2} classics!",
+            "When not watching {genre1}, you'll find me exploring {genre2} hits.",
+            "Always up for a great {genre1} or {genre2} movie!"
+        ]
+        locations = [
+            "New York, USA", "Los Angeles, USA", "London, UK", "Tokyo, Japan",
+            "Paris, France", "Berlin, Germany", "Sydney, Australia", "Toronto, Canada",
+            "Mumbai, India", "Seoul, South Korea", "Mexico City, Mexico", "Rome, Italy"
+        ]
+        website_templates = [
+            "https://moviefan{num}.example.com",
+            "https://cinemalover{num}.example.org",
+            "https://filmcritics{num}.example.net",
+            "https://movienight{num}.example.io"
+        ]
+
+        try:
+            with transaction.atomic():
+                # 1) Bulk-create the users
+                created_users = User.objects.bulk_create(users_to_create, batch_size=len(users_to_create))
+                result["created"] = len(created_users)
+
+                # 2) Bulk-create the corresponding profiles
+                profiles_to_create = []
+                for user in created_users:
+                    # Randomly choose 2 "fake" genres, or real ones if they exist
+                    if all_genres:
+                        selected_genres = random.sample(all_genres, min(len(all_genres), random.randint(2, 4)))
+                        g1 = selected_genres[0].name
+                        g2 = selected_genres[1].name if len(selected_genres) > 1 else "Drama"
+                    else:
+                        g1, g2 = "Action", "Drama"
+
+                    bio = random.choice(bio_templates).format(genre1=g1, genre2=g2)
+                    loc = random.choice(locations)
+                    web = random.choice(website_templates).format(num=user_index_map[user.username])
+
+                    profiles_to_create.append(
+                        UserProfile(
+                            user=user,
+                            bio=bio,
+                            location=loc,
+                            website=web,
+                            profile_pic=profile_pic_url
+                        )
+                    )
+
+                UserProfile.objects.bulk_create(profiles_to_create, batch_size=len(profiles_to_create))
+
+                # 3) Optionally assign favorite genres if desired (M2M). This is typically
+                #    done with multiple insert statements, but can be done in a second pass:
+                # for profile, selected_genres in zip(profiles_to_create, <something>):
+                #     profile.favorite_genres.add(*selected_genres)
+
+        except Exception as e:
+            # If the transaction fails, mark them all as errors
+            result["errors"] = len(users_to_create)
+
+        return result
