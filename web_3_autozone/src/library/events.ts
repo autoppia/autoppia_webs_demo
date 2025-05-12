aad79e9e53c9e6983ed30251cddaa43696d1dcd8
khushbakht <khushbakht.tahir@rev9solutions.com>
2025-05-12 14:55:46 +0500

    changed order of events

--- web_3_autozone/src/library/events.ts
+++ web_3_autozone/src/library/events.ts
@@ -1,15 +1,15 @@
 // src/lib/logger.ts
 
 export const EVENT_TYPES = {
-  SEARCH_PRODUCT: "SEARCH_PRODUCT", 
-  ADD_TO_CART: "ADD_TO_CART",  
-  CHECKOUT_STARTED: "CHECKOUT_STARTED", 
-  VIEW_CART: "VIEW_CART", 
-  SCROLL_CAROUSEL: "CAROUSEL_SCROLL", 
-  VIEW_DETAIL: "VIEW_DETAIL", 
-  QUANTITY_CHANGED: "QUANTITY_CHANGED", 
-  ORDER_COMPLETED: "ORDER_COMPLETED", 
-  PROCEED_TO_CHECKOUT:"PROCEED_TO_CHECKOUT"
+  SCROLL_CAROUSEL: "CAROUSEL_SCROLL",
+  SEARCH_PRODUCT: "SEARCH_PRODUCT",
+  VIEW_DETAIL: "VIEW_DETAIL",
+  ADD_TO_CART: "ADD_TO_CART",
+  CHECKOUT_STARTED: "CHECKOUT_STARTED",
+  VIEW_CART: "VIEW_CART",
+  QUANTITY_CHANGED: "QUANTITY_CHANGED",
+  PROCEED_TO_CHECKOUT: "PROCEED_TO_CHECKOUT",
+  ORDER_COMPLETED: "ORDER_COMPLETED",
 } as const;
 
 export type EventType = (typeof EVENT_TYPES)[keyof typeof EVENT_TYPES];
@@ -17,11 +17,10 @@ export type EventType = (typeof EVENT_TYPES)[keyof typeof EVENT_TYPES];
 // Mock user/agent — replace with actual session/user context if available
 
 export function logEvent(event: EventType, data: any = {}) {
-    
-    if (typeof window === "undefined") return;
-  
-    const user = localStorage.getItem("user");
-    const web_agent_id = localStorage.getItem("web_agent_id");
+  if (typeof window === "undefined") return;
+
+  const user = localStorage.getItem("user");
+  const web_agent_id = localStorage.getItem("web_agent_id");
   const payload = {
     event,
     data,
