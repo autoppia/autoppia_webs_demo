8a522f54553f99c2cd9ce167edc431937f5198c4
khushbakht <khushbakht.tahir@rev9solutions.com>
2025-05-14 13:08:56 +0500

    resolved build issues

--- web_3_autozone/src/app/layout.tsx
+++ web_3_autozone/src/app/layout.tsx
@@ -6,11 +6,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Autozon - Online Shopping for Electronics, Apparel, and More",
-  description: "Shop online for the best deals on electronics, clothing, books, and much more at Autozon.",
+  description:
+    "Shop online for the best deals on electronics, clothing, books, and much more at Autozon.",
 };
 
 export default function RootLayout({
