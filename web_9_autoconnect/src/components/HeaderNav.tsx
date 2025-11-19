bb0e690da267ade9fc7d73ce62b34ca92645a800
dr riiveer <59394472+Riiveer@users.noreply.github.com>
2025-11-19 17:15:16 +0100

    update

--- web_9_autoconnect/src/components/HeaderNav.tsx
+++ web_9_autoconnect/src/components/HeaderNav.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { SeedLink } from "@/components/ui/SeedLink";
-import Image from "next/image";
 import { usePathname } from "next/navigation";
 import UserSearchBar from "./UserSearchBar";
 import { logEvent, EVENT_TYPES } from "@/library/events";
