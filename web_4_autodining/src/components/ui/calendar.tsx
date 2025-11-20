e076d4151e83bb694672115666492aae79d7d9f8
Tayyab Cheema <tayyab.cheema@rev9solutions.com>
2025-11-20 11:47:04 +0500

    Fixed the home page not being rendered in web4

--- web_4_autodining/src/components/ui/calendar.tsx
+++ web_4_autodining/src/components/ui/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { buttonVariants } from "@/components/ui/button"
-import { cn } from "@/dynamic/v1-layouts"
+import { cn } from "@/library/utils"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
